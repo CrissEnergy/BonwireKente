8242bbb5d52048f17fd2b3e69932bd51d1d9862b
Criss Energy <crissenergy1@gmail.com>
2025-11-14 23:45:08 +0000

    Try fixing this error: `Console Error: A param property was accessed dir

--- src/app/shop/[slug]/page.tsx
+++ src/app/shop/[slug]/page.tsx
@@ -1,7 +1,7 @@
 
 'use client';
 
-import { notFound } from 'next/navigation';
+import { notFound, useParams } from 'next/navigation';
 import Image from 'next/image';
 import { ProductCard } from '@/components/products/ProductCard';
 import { Separator } from '@/components/ui/separator';
@@ -15,8 +15,9 @@ import { doc, collection } from 'firebase/firestore';
 import type { Product } from '@/lib/types';
 import { Loader2 } from 'lucide-react';
 
-export default function ProductPage({ params }: { params: { slug: string } }) {
-  const slug = params.slug;
+export default function ProductPage() {
+  const params = useParams();
+  const slug = params.slug as string;
   const heroImage = PlaceHolderImages.find(img => img.id === 'hero-image');
   const firestore = useFirestore();
 
